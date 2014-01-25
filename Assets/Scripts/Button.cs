13cd86ff3b9e4806228df148be293262809bdeeb
EveryDayPersonified <student@AIE-SEA-SK019.aie.local>
2014-01-25 11:44:28 -0800

    Menu Scripts

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Button : MonoBehaviour {
+	Action clickAction;
+	public bool IsSelected;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = transform.GetComponent<Animator>();
+		animator.StartPlayback();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Menu : MonoBehaviour {
+	public List<Button> buttons;
+	int selectedIndex;
+	public bool horizontalPressed;
+
+	// Use this for initialization
+	void Start () {
+		buttons = new List<Button>();
+		selectedIndex = 0;
+		horizontalPressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!horizontalPressed &&
+		    Input.GetAxis("Horizontal") > 1)
+		{
+			selectedIndex++;
+			horizontalPressed = true;
+		}
+
+		if (!horizontalPressed &&
+		    Input.GetAxis("Horizontal") < 1)
+		{
+			selectedIndex--;
+			horizontalPressed = true;
+		}
+
+		if (horizontalPressed &&
+		    Input.GetAxis("Horizontal") == 0)
+			horizontalPressed = false;
+	}
+}
